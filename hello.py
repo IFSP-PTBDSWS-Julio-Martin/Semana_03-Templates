8e1bdeb3ab9f8fd40c39f6cd9017479441d86921
JulioPMartin <julio.martin@aluno.ifsp.edu.br>
2025-08-28 16:43:37 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,28 +3,20 @@ from flask import Flask
 from flask import request
 from flask import redirect
 from flask import abort
+from flask import render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 @app.route('/')
 def home():
-    return '''
-    <h1>Avaliação contínua: Aula 030</h1>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/user/Julio%20Pereira%20Martin/PT3031608/IFSP">Identificação</a></li>
-        <li><a href="/contextorequisicao">Contexto da requisição</a></li>
-    </ul>
-    '''
-@app.route('/user/Julio%20Martin')
+    return render_template('index.html)
+    
+@app.route('/user/<nome>')
 def user():
-    return '''
-
-    '''
+    return render_template('user.html', nome=nome)
 
 @app.route('/rotainexistente')
 def erro():
-    return '''
-
-    '''
-bootstrap = Bootstrap(app)
+    return render_template('404.html') 
